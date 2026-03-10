664604e6ddf0893ed3487f8a7cf37c4299a8d412
Kuldeep Pal <kuldeep27396@gmail.com>
2026-03-10 18:55:23 +0530

    Clean up imports in test_review.py
    Removed unused imports from test_review.py.

--- tests/test_review.py
+++ tests/test_review.py
@@ -11,16 +11,6 @@ from pr_review_agent.models import (
     PullRequestWebhookPayload,
     ReviewIssue,
     ReviewOutput,
-)
-from pr_review_agent.pr_review_graph import (
-    PRReviewGraphState,
-    route_after_file_fetch,
-    route_after_incremental_scope,
-    route_after_review_generation,
-    route_after_rule_evaluation,
-)
-from pr_review_agent.review import ReviewService, extract_added_lines
-
 
 class DummySettings:
     review_simple_changes = False
